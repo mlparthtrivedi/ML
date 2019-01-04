4f0bb30af7fc7b202fdde2aad2a25fead3568502
mlparthtrivedi <parthtrivedi003@gmail.com>
2019-01-05 01:12:47 +0530

    Change index

--- Regression/Decision Tree Regression/decision_tree_regression.py
+++ Regression/Decision Tree Regression/decision_tree_regression.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+y = dataset.iloc[:, 1:2].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
